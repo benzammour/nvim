2675c5d6a021f75422dd5867c9e8b19e97714f4f
Samir Benzammour <samir.benzammour@gmail.com>
2024-10-18 06:41:11 +0200

    chore: remove markdownlint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,13 +1,13 @@
 return {
-
   {
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+      lint.linters_by_ft['markdown'] = nil
+      --lint.linters_by_ft = {
+      --  markdown = { 'markdownlint' },
+      --}
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
