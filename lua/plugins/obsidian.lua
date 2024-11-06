1c39307f11cd9d431057ee177cd512d24c069166
Samir Benzammour <samir.benzammour@gmail.com>
2024-11-06 13:40:56 +0100

    feat: add tempalte dir

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -3,14 +3,6 @@ return {
   version = '*',
   lazy = true,
   ft = 'markdown',
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
@@ -23,5 +15,13 @@ return {
         path = '~/Vault/',
       },
     },
+  },
+  -- Optional, for templates (see below).
+  templates = {
+    folder = 'z_Templates',
+    date_format = '%Y-%m-%d',
+    time_format = '%H:%M',
+    -- A map for custom variables, the key should be the variable and the value a function
+    substitutions = {},
   },
 }
