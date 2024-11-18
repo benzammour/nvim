81b631bdafdebd892f4e6f6790876ad2ebad4864
Samir Benzammour <samir.benzammour@gmail.com>
2024-11-19 00:38:00 +0100

    feat: add formatters

--- init.lua
+++ init.lua
@@ -291,12 +291,11 @@ require('lazy').setup({
         }
       end,
       formatters_by_ft = {
+        c = { 'clang_format' },
+        cpp = { 'clang_format' },
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        python = { 'ruff', 'black', stop_after_first = true },
+        tex = { 'latexindent' },
       },
     },
   },
