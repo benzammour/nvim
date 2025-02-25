62b589d8b2dd448fe4bac72b495dc166e9d6c15b
Samir Benzammour <samir.benzammour@gmail.com>
2025-02-25 08:18:08 +0100

    feat: change copy-file-path command

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader>p', function()
+vim.keymap.set('n', '<leader>yp', function()
   local file_dir = vim.fn.expand '%:p:h'
   if file_dir ~= '' then
     vim.fn.setreg('+', file_dir) -- Copies the directory path to the clipboard
