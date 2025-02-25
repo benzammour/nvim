23e800f7450a59095b1c72671f016f6c8061fb53
Samir Benzammour <samir.benzammour@gmail.com>
2025-02-25 08:17:27 +0100

    feat: add alpha default screen

--- init.lua
+++ init.lua
@@ -501,6 +501,7 @@ require('lazy').setup({
   require 'plugins.zen-mode',
   require 'plugins.pastify',
   require 'plugins.lazygit',
+  require 'plugins.alpha',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
