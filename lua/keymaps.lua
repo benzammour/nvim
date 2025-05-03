5bcb91adb66a40188ed5c9a54173f06ecd8ba4f2
Samir Benzammour <samir.benzammour@gmail.com>
2025-05-04 01:21:05 +0200

    feat: add leader-ll for lazy menu

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,6 +22,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>ll', '<cmd>Lazy<cr>', { desc = 'Open Lazy Package Manager Menu' })
+
 vim.keymap.set('n', '<leader>yp', function()
   local file_dir = vim.fn.expand '%:p:h'
   if file_dir ~= '' then
