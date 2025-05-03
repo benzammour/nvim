9f392b85a56b214dad1018affba7808f3cf43233
Samir Benzammour <samir.benzammour@gmail.com>
2025-05-04 01:20:55 +0200

    feat: pin telescope version to latest stable

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  tag = '0.1.8',
+  --branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
