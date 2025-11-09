9e7735c52ee198a282a22eebba748859baa47c79
Samir Benzammour <samir.benzammour@gmail.com>
2025-11-09 23:13:21 +0100

    feat: add conform keybinds and additional filetypes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,21 +4,72 @@ return { -- Autoformat
   cmd = { 'ConformInfo' },
   keys = {
     {
-      '<leader>f',
+      '<leader>ff',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
       mode = '',
       desc = '[F]ormat buffer',
     },
+    {
+      '<leader>fm',
+      function()
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        -- List of available filetypes for selection
+        local filetypes = {
+          'c',
+          'cpp',
+          'golang',
+          'css',
+          'html',
+          'json',
+          'yaml',
+          'markdown',
+          'lua',
+          'python',
+          'xml',
+        }
+
+        -- Always ask the user to pick a filetype from the list
+        vim.ui.select(filetypes, { prompt = 'Select filetype:' }, function(choice)
+          if choice then
+            -- Set the filetype based on user selection
+            vim.bo[bufnr].filetype = choice
+            vim.notify('Filetype set to: ' .. choice)
+
+            -- Format the buffer based on the selected filetype
+            require('conform').format {
+              lsp_fallback = lsp_fallback,
+              async = async,
+              bufnr = bufnr,
+              timeout_ms = formatting_timeout,
+            }
+          else
+            vim.notify('No filetype selected. Aborting formatting.', vim.log.levels.WARN)
+          end
+        end)
+      end,
+      desc = 'Format file with filetype selection',
+    },
   },
   opts = {
     notify_on_error = false,
+    formatters_by_ft = {
+      c = { 'clang_format' },
+      cpp = { 'clang_format' },
+      lua = { 'stylua' },
+      python = { 'isort', 'black' },
+      tex = { 'latexindent' },
+      yaml = { 'prettier' },
+      rust = { 'rustfmt', lsp_format = 'fallback' },
+      xml = { 'xmlformatter' },
+      markdown = { 'prettier' },
+      json = { 'jq' },
+    },
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      --local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = {}
       local lsp_format_opt
       if disable_filetypes[vim.bo[bufnr].filetype] then
         lsp_format_opt = 'never'
@@ -30,13 +81,5 @@ return { -- Autoformat
         lsp_format = lsp_format_opt,
       }
     end,
-    formatters_by_ft = {
-      c = { 'clang_format' },
-      cpp = { 'clang_format' },
-      lua = { 'stylua' },
-      python = { 'black', stop_after_first = true },
-      tex = { 'latexindent' },
-      yaml = { 'prettier' },
-    },
   },
 }
