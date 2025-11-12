dbfa475e71a2c022f28f12b2df4b5128b09a2590
Samir Benzammour <samir.benzammour@gmail.com>
2025-11-12 10:36:20 +0100

    feat(plugins): add default config and keybinds

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,5 @@
 return {
   'folke/trouble.nvim',
-  opts = {},
   cmd = 'Trouble',
   keys = {
     {
@@ -24,4 +23,15 @@ return {
       desc = 'Quickfix List (Trouble)',
     },
   },
+  config = function()
+    require('trouble').setup {}
+
+    vim.keymap.set('n', '[t', function()
+      require('trouble').next { skip_groups = true, jump = true }
+    end)
+
+    vim.keymap.set('n', ']t', function()
+      require('trouble').previous { skip_groups = true, jump = true }
+    end)
+  end,
 }
